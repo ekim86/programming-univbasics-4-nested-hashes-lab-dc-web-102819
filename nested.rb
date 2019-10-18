d2c6fee5ec625d5c52ea60dd3689e039f2ee62fe
eunice kim <oonis86@gmail.com>
2019-10-18 17:27:53 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -16,8 +16,7 @@ def hopper
         }
      }
 
-  programmer_hash[:known_for] = "COBOL"
-  programmer_hash[:languages] = ["COBOL", "FORTRAN"]
+  programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
